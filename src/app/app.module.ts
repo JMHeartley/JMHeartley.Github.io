a55024cd982410e9a9dfd2559242a1aa8ba4cae2
Justin <heartleyjm@gmail.com>
2023-01-24 22:13:54 -0500

    REFACTOR: extract dtree-seed details into component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { ProjectComponent } from './project/project.component';
+import { DtreeSeedComponent } from './project/dtree-seed/dtree-seed.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    ProjectComponent
+    ProjectComponent,
+    DtreeSeedComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/project/dtree-seed/dtree-seed.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dtree-seed',
+  templateUrl: './dtree-seed.component.html'
+})
+export class DtreeSeedComponent {
+}
