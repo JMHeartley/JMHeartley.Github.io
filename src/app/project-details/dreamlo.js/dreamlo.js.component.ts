a1a22d103247c9de01223a5f3480b50ad206e5f2
Justin <heartleyjm@gmail.com>
2023-01-27 17:36:40 -0500

    FIX: correct description for Work With Me Here

--- src/app/project-details/dreamlo.js/dreamlo.js.component.ts
+++ src/app/project-details/dreamlo.js/dreamlo.js.component.ts
@@ -7,7 +7,7 @@ import { Project } from "src/app/shared/project.model";
 })
 export class DreamloJsComponent implements Project {
   bannerFilePath: string = 'assets/img/dreamlo.js/main.png';
-  description: string = 'A Javascript library for creating, retrieving, updating, and deleting scores on your dreamlo leaderboard via its web API.';
   bannerAltText: string = 'dreamlo.js and dreamlo UI logos';
+  description: string = 'A library for creating, retrieving, updating, and deleting scores on your dreamlo leaderboard via its web API.';
   tags: string[] = ['open-source', 'library', 'typescript', 'javascript', 'html', 'css', 'bootstrap'];
 }
