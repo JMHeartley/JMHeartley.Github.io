f04ee3c15a4b312bb0d968e726e59d28e3f3df68
Justin <heartleyjm@gmail.com>
2023-02-08 13:53:02 -0500

    FEAT: add more details to dreamlo.js page
    * added more details to the dreamlo.js page
    * updated styles to match new theme
    * added screenshots to go with text

--- src/app/project-details/dreamlo.js/dreamlo.js.component.ts
+++ src/app/project-details/dreamlo.js/dreamlo.js.component.ts
@@ -10,5 +10,5 @@ export class DreamloJsComponent implements Project {
   bannerFilePath = 'assets/img/dreamlo.js/main.png';
   bannerAltText = 'dreamlo.js and dreamlo UI logos';
   description = 'A library for creating, retrieving, updating, and deleting scores on your dreamlo leaderboard via its web API.';
-  tags: string[] = ['open-source', 'library', 'typescript', 'javascript', 'html', 'css', 'bootstrap'];
+  tags: string[] = ['open-source', 'library', 'typescript', 'javascript', 'api', 'html', 'css', 'bootstrap'];
 }
