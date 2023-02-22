77d643674cbd21886df302abb11b03bc5cb88411
Justin <heartleyjm@gmail.com>
2023-02-21 20:32:15 -0500

    FEAT: add on load animations to project list

--- src/app/shared/project-list/project-list.component.ts
+++ src/app/shared/project-list/project-list.component.ts
@@ -1,15 +1,17 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, AfterViewInit } from '@angular/core';
 import { DtreeSeedComponent } from 'src/app/project-details/dtree-seed/dtree-seed.component';
 import { PicsOfUsComponent } from 'src/app/project-details/pics-of-us/pics-of-us.component';
 import { DreamloJsComponent } from 'src/app/project-details/dreamlo.js/dreamlo.js.component';
 import { WorkWithMeHereComponent } from 'src/app/project-details/work-with-me-here/work-with-me-here.component';
 import { Project } from "../project.model";
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-project-list',
   templateUrl: './project-list.component.html'
 })
-export class ProjectListComponent implements OnInit, OnChanges {
+export class ProjectListComponent implements OnInit, OnChanges, AfterViewInit {
+  @Input() timeline: gsap.core.Timeline;
   @Input() tagFilter: string;
   allProjects: Project[] = [
     new PicsOfUsComponent,
@@ -35,6 +37,20 @@ export class ProjectListComponent implements OnInit, OnChanges {
 
     this.projectsWithMatchingTag = this.allProjects.filter(project => {
       return project.tags.includes(this.tagFilter);
+    });
+  }
+  ngAfterViewInit() {
+    if (!this.timeline) {
+      this.timeline = gsap.timeline();
+    }
+
+    this.timeline.from('.theme-card', {
+      duration: 0.5,
+      opacity: 0,
+      stagger: 0.5,
+      y: -20,
+      scale: 0.5,
+      ease: 'power2.out'
     });
   }
 }
