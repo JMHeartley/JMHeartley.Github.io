adb99263232bea9d8a4b4ff75fcb8183a81bc850
Justin <heartleyjm@gmail.com>
2023-02-21 20:31:57 -0500

    FEAT: add on load animations to home page
    * added greensock to the project
    * added on load animations to the home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { gsap } from 'gsap';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  timeline: gsap.core.Timeline;
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.timeline = gsap.timeline()
+      .from('#hi', { delay: 0.5, duration: 1.5, opacity: 0 })
+      .from('#justin', { delay: 0.5, duration: 1.5, opacity: 0 })
+      .from('#subtitle', { delay: 0.25, duration: 0.5, opacity: 0, y: -5 })
+      .from('#check-out', { delay: 0.25, duration: 0.75, opacity: 0, y: -5 })
+      .from('#arrow', { duration: 0.75, opacity: 0 })
+      .from('#arrow', { duration: 0.75, repeat: -1, yoyo: true, y: -5 });
+  }
 }
