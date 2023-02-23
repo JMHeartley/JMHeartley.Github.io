2c669aa6b01806eb51f0066a9b04573e7ae9d9ef
Justin <heartleyjm@gmail.com>
2023-02-22 20:33:49 -0500

    FIX: remove >100vw div before scale animation
    * using scale: 2 on a div was making the div 200% pre-animation (since divs are 100% width x 2 scale)
    * replaced scale animation with font-size

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,7 +16,7 @@ export class HomeComponent implements OnInit {
       .from('#hi', { delay: 0.25, duration: 1, opacity: 0 })
       .from('#justin', { delay: 0.5, duration: 1, opacity: 0 })
       .from('#subtitle', { delay: 0.25, duration: 0.5, opacity: 0, y: -5 })
-      .from('#check-out', { delay: 0.25, duration: 0.75, opacity: 0, scale: 2, ease: 'bounce.out' })
+      .from('#check-out', { delay: 0.25, duration: 0.75, opacity: 0, fontSize: '2rem', ease: 'bounce.out' })
       .from('#arrow', { duration: 0.5, opacity: 0 })
       .from('#arrow', { duration: 0.75, repeat: -1, yoyo: true, y: -5, ease: 'sine.inOut' });
   }
